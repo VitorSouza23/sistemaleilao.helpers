c1e1172b234c4ede02828f45ccf863db3d82ab7a
VitorSouza23 <vitordesolza@gmail.com>
2017-11-27 22:52:44 -0200

    Adição do helper multicast

---
+++ src/helpers/multcastconfig/MultcastConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers.multcastconfig;
+
+/**
+ *
+ * @author Vitor
+ */
+public class MultcastConfig {
+    public static final String MULTCAST_ADDRESS = "228.5.6.78";
+    public static final int MULTCAST_PORT = 50003;
+    public static final int MULTCAST_MESSAGE_LENGTH =  "1".getBytes().length;
+    public static final String YES_MESSAGE = "1";
+    public static final String NO_MESSAGE = "0";
+}
